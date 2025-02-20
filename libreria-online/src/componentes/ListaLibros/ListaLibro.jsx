36f9117b854b1c28d356cc02e1a923785c22a86b
HarryDredd <harrisonpaterninapvz@gmail.com>
2025-02-20 14:15:01 -0500

    datos-se-muestran-en-consola-v1.2

--- libreria-online/src/componentes/ListaLibros/ListaLibro.jsx
+++ libreria-online/src/componentes/ListaLibros/ListaLibro.jsx
@@ -1,10 +1,41 @@
 import React from "react";
+import { useGlobalContext } from '../../consApi';
+import Libro from "../ListaLibros/Libro";
+import Loading from "../Cargador/Cargador";
+import coverImg from "../../Imagenes/cover_not_found.jpg";
+import "./listalibro.css";
+
+//https://covers.openlibrary.org/b/id/240727-S.jpg  cover
 
 const ListaLibro = () => {
+    const {books, loading, resultTitle} = useGlobalContext();
+    const booksWithCovers = books.map((singleBook) => {
+        return {
+            ...singleBook,
+            id: (singleBook.id).replace("/wprks/", ""),
+            cover_img: singleBook.cover_id ? `https://covers.openlibrary.org/b/id/${singleBook.cover_id}-L.jpg` : coverImg
+        }
+    });
+
+    
+
+    if(loading) return <Loading />
+
     return (
-        <div>
-            Lista De Libro
+       <section className="booklist">
+        <div className="container">
+            <h2>{resultTitle}</h2>
         </div>
+        <div className="booklist-content grid">
+            {
+                booksWithCovers.slice(0, 30).map((item, index) =>{
+                    return (
+                        <Libro key = {index} { ... item} />
+                    )
+                })
+            }
+        </div>
+       </section>
     )
 }
 
